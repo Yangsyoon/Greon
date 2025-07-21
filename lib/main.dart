d88ae1ee712b9f09df10f68055e6ada5e017d9c2
jsw <wkd8642@naver.com>
2025-07-21 20:08:18 +0900

    fail

--- lib/main.dart
+++ lib/main.dart
@@ -9,25 +9,93 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_phoenix/flutter_phoenix.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:get_it/get_it.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:http/http.dart' as http;
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
+import 'application/delivery_info_fetch_cubit/delivery_info_fetch_cubit.dart';
 import 'application/post_bloc/post_bloc.dart';
 import 'application/post_bloc/post_event.dart';
 import 'core/app/app.dart';
+import 'core/networkchecker/network_info.dart';
 import 'core/observer/bloc_observer.dart';
+import 'data/data_sources/local/delivery_info_local_data_source.dart';
 import 'data/data_sources/local/user_local_data_source.dart';
+import 'data/data_sources/remote/delivery_info_remote_data_source.dart';
+import 'data/repositories/delivery_info_impl.dart';
 import 'data/repositories/post_repository.dart';
 import 'di/cubits.dart';
 import 'di/di.dart' as di;
 import 'di/product.dart';
+import 'domain/repositories/delivery_info_repository.dart';
+import 'domain/usecases/delivery_info/clear_local_delivery_info_usecase.dart';
+import 'domain/usecases/delivery_info/get_cached_delivery_info_usecase.dart';
+import 'domain/usecases/delivery_info/get_remote_delivery_info_usecase.dart';
+import 'domain/usecases/delivery_info/get_selected_delivery_info_usecase.dart';
 import 'firebase_options.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+final sl = GetIt.instance;
+
+// 반드시 async
+Future<void> setupLocator() async {
+  // 외부 의존성 등록
+  final sharedPreferences = await SharedPreferences.getInstance();
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => const FlutterSecureStorage());
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+
+  // NetworkInfo 등록 (예: DataConnectionChecker 기반)
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl<InternetConnectionChecker>()));
+
+  // UserLocalDataSource 등록
+  sl.registerLazySingleton<UserLocalDataSource>(
+        () => UserLocalDataSourceImpl(
+      sharedPreferences: sl(),
+      secureStorage: sl(),
+    ),
+  );
+
+  // DataSource 등록
+  sl.registerLazySingleton<DeliveryInfoRemoteDataSource>(
+        () => DeliveryInfoRemoteDataSourceImpl(client: sl()),
+  );
+  sl.registerLazySingleton<DeliveryInfoLocalDataSource>(
+        () => DeliveryInfoLocalDataSourceImpl(sharedPreferences: sl()),
+  );
+
+  // Repository 등록
+  sl.registerLazySingleton<DeliveryInfoRepository>(
+        () => DeliveryInfoRepositoryImpl(
+      remoteDataSource: sl(),
+      localDataSource: sl(),
+      userLocalDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+
+  // UseCase 등록
+  sl.registerLazySingleton(() => GetRemoteDeliveryInfoUseCase(sl()));
+  sl.registerLazySingleton(() => GetCachedDeliveryInfoUseCase(sl()));
+  sl.registerLazySingleton(() => GetSelectedDeliveryInfoInfoUseCase(sl()));
+  sl.registerLazySingleton(() => ClearLocalDeliveryInfoUseCase(sl()));
+
+  // Cubit 등록
+  sl.registerFactory(() => DeliveryInfoFetchCubit(
+    sl<GetRemoteDeliveryInfoUseCase>(),
+    sl<GetCachedDeliveryInfoUseCase>(),
+    sl<GetSelectedDeliveryInfoInfoUseCase>(),
+    sl<ClearLocalDeliveryInfoUseCase>(),
+  ));
+}
 
 /// 1. FCM 백그라운드 핸들러
 @pragma('vm:entry-point')
@@ -166,6 +234,7 @@ Future<void> _setupFCM(BuildContext context) async {
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  setupLocator();
 
   tz.initializeTimeZones();
   await GetStorage.init();
